bd673d8289dc63c74b2fdba5318f0c2fdb8f12a2
BeainaT <tania.df@hotmail.it>
2022-05-16 15:21:59 +0200

    added posts commented html

--- main.js
+++ main.js
@@ -65,5 +65,45 @@ const posts = [
 // 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 
 /* FUNCTIONS */
+function addPost(obj, to) {
+    const post =
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${obj["author"].image}" alt="${obj["author"].name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${obj["author"].name}</div>
+                    <div class="post-meta__time">${obj.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${obj.content}</div>
+        <div class="post__image">
+            <img src="${obj.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${obj.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${obj.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+    to.innerHTML += post;
+}
 
 /* MAIN */
+const postsContainer = document.querySelector("#container");
+for (let i=0; i < posts.length; i++) {
+    let info = posts[i];
+    addPost(info, postsContainer);
+}
